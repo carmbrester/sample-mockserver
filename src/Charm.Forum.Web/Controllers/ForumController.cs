a4ec29e73b062008b78e7ec5b9391a720e597e7a
Armbrester, Christopher E <CHRISTOPHER_E_ARMBRESTER@homedepot.com>
2021-10-08 10:40:41 -0400

    Sample project pushed.
    May be issues with the renaming from 4um to Forum.

---
+++ src/Charm.Forum.Web/Controllers/ForumController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charm.Forum.ForumServiceClient.Api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charm.Forum.Web.Controllers
+{
+    public class ForumController : Controller
+    {
+        // GET: ForumController
+        public async Task<IActionResult> Index()
+        {
+            var client = new PostsApi(Environment.GetEnvironmentVariable("FORUM_SERVICE_URL"));
+            var posts = await client.GetPostsAsync();
+            return Ok(posts);
+        }
+    }
+}
